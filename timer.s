90a7956f4af6c16635c9e20f9db298aac29cdac3
Karen Weng <karen.wengxt@gmail.com>
2025-03-06 17:58:45 -0500

    updated timer not finished

--- timer.s
+++ timer.s
@@ -13,6 +13,7 @@ la s0, SW_BASE
 la s1, KEY_BASE
 la s2, TIMER_BASE
 la s3, LED_BASE
+la s4, study
 
 
 
@@ -20,20 +21,28 @@ la sp, 0x20000
 
 #interrupts
 csrw mstatus, x0
+li a0, 0 #initially study
+call setup_key
+call setup_timer
 
 setup_key:
     li t0, 0xF
     sw t0, 8(s1)
     sw t0, 12(s1)
+    ret
 
 setup_timer:
-    li t0, 12000000 #25 min
+    #a0 0 if study, a0 4 if break
+    and t0, s4, a0
+    lw t0, 0(t0) 
+    # li t0, 12000000 #25 min
     # ^probably want to change this line to memory in the future
     lw t0, 8(s2)
     srli t0, t0, 16
     lw t0, 12(s2)
-    li t0, 0b1001
+    li t0, 0b0101 # start and interrupt
     lw t0, 0(s0)
+    ret
 
 #cpu allow interupts form
 li t0, 0x50000 #key and timer
@@ -78,15 +87,71 @@ key_interrupt:
 
 key0:
 #set/start timer
+# 
+lw t0, 0(s0) #lw switch value
+li t3, 480000
+
+andi t1, t0, 0xF #break time
+mul t1, t1, t3
+sw t1, 4(s4)
+
+srli t1, t0, 4 #study time
+mul t1, t1, t3
+sw t1, 0(s4)
+
+beqz a0, break
+li a0, 0
+call setup_timer
+
+break:
+li a0, 4
+call setup_timer
+
 key1:
+lw t0, 0(s1) 
+andi, t0, t0, 2 # run bit check if timer is running or paused
+bgtz t0, pause_timer
+#timer paused
+li t0, 0(s3) 
+
+
+li t0, 4
+j load_pause
+
+pause_timer: # timer current running
+li t0, 8
+
+load_pause:
+lw t0, 4(s2)
+bgtz t0, stop_alarm
+
+stop_alarm:
+sw x0, 0(s3) # turn off all leds
+
+
+
 #pause/start timer
+
+
 key2:
 #reset timer
+call setup_timer
+
 key3:
 
 
-timer_interrupt:
 
+
+timer_interrupt:
+li t0, -1 
+sw t0, 0(s3) #turn on all leds
+
+
+
+
+.data 
+study: .word 12000000 #25min
+break: .word 2400000 #5min
 
 
 
