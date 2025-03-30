c50b858bc2bd5a9315212b5747e7bf6098296d03
Karen Weng <karen.wengxt@gmail.com>
2025-03-29 20:36:51 -0400

    Update working_hourglass.c

--- header file/working_hourglass.c
+++ header file/working_hourglass.c
@@ -533,7 +533,7 @@ void audio_ISR_timer2(void)
             play_audio_samples(boo_44100_samples, boo_44100_num_samples, &boo_44100_index);
         }
         else if (key_mode == 3) { // alarm
-            play_audio_samples(rooster_samples, rooster_num_samples, &rooster_sample_index);
+            play_audio_samples(rooster_samples, rooster_num_samples, &rooster_index);
         }
     }
 
