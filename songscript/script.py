9461b3f3c20672a672d058078a34cab951eaacfc
Karen Weng <karen.wengxt@gmail.com>
2025-03-29 20:04:38 -0400

    fluffing a duck in header

--- songscript/script.py
+++ songscript/script.py
@@ -2,14 +2,18 @@ import numpy as np
 
 if __name__ == "__main__":
     # Read raw PCM file
-    filename = "evenSHORTLofi.raw"
+    filename_beginning = "fluffing_duck_30sec_44100"
+    filename = filename_beginning+".raw"
     data = np.fromfile(filename, dtype=np.uint32)  # Read as 32-bit unsigned integers
 
     # Convert to C array format
-    with open("evenSHORTLofiSamples.h", "w") as f:
-        f.write("int samples[] = {\n")
+    with open("samples_"+filename_beginning+".h", "w") as f:
+
+        f.write(f"int {filename_beginning}_index = 0;\n")
+        f.write("int "+filename_beginning+"_samples[] = {\n")
+
         f.write(",\n".join(f"    0x{sample:08x}" for sample in data))
         f.write("\n};\n")
-        f.write(f"int samples_n = {len(data)};\n")
+        f.write(f"int {filename_beginning}_num_samples = {len(data)};\n")
 
-        print("C array saved to evenSHORTERLofiSamples.h")
+        print("C array saved to", filename_beginning)
