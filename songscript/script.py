86a8b2462295569142cbb236c4a33c1da8d20e8b
Karen Weng <karen.wengxt@gmail.com>
2025-03-30 17:33:39 -0400

    2 types of keybaord clicks

--- songscript/script.py
+++ songscript/script.py
@@ -2,7 +2,7 @@ import numpy as np
 
 if __name__ == "__main__":
     # Read raw PCM file
-    filename_beginning = "beep_beep_louder_44100"
+    filename_beginning = "keyboard_click_2_44100"
     filename = filename_beginning+".raw"
     data = np.fromfile(filename, dtype=np.uint32)  # Read as 32-bit unsigned integers
 
